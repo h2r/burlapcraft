50a9514cbc0c87148310793b66b34d99245bd86a
kcaluru <krishna_aluru@brown.edu>
2015-04-01 19:09:40 -0400

    added multiple wands, created new dungeon
    Former-commit-id: 6525906c22b7380d8ec3a9a5b46c1fc6b4d9b12d

--- forge/src/main/java/com/kcaluru/burlapbot/BurlapMod.java
+++ forge/src/main/java/com/kcaluru/burlapbot/BurlapMod.java
@@ -1,22 +1,30 @@
 package com.kcaluru.burlapbot;
 
-import com.kcaluru.burlapbot.items.ItemBurlapWand;
+import com.kcaluru.burlapbot.blocks.BlockBurlapStone;
+import com.kcaluru.burlapbot.items.ItemBridgeWand;
+import com.kcaluru.burlapbot.items.ItemFinderWand;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.IWorldGenerator;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = BurlapMod.MODID, version = BurlapMod.VERSION)
-public class BurlapMod
-{
+public class BurlapMod {
+	
 	// mod information
     public static final String MODID = "burlapmod";
     public static final String VERSION = "1.0";
@@ -25,24 +33,42 @@ public class BurlapMod
     public static boolean structCreated = false;
     
     // items
-    public static Item wand;
+    public static Item finderWand;
+    public static Item bridgeWand;
+    
+    // blocks
+    public static Block burlapStone;
     
     // event handlers
-    BurlapEventHandler handler = new BurlapEventHandler();
+    BurlapWorldGenHandler genHandler = new BurlapWorldGenHandler();
+    BurlapEventHandler eventHandler = new BurlapEventHandler();
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-    	wand = new ItemBurlapWand();
+    	
+    	finderWand = new ItemFinderWand();
+    	bridgeWand = new ItemBridgeWand();
+    	burlapStone = new BlockBurlapStone();
     	
     	// make sure minecraft knows
-    	GameRegistry.registerItem(wand, "burlapwand");
-    	GameRegistry.registerWorldGenerator(handler, 0);
+    	GameRegistry.registerItem(finderWand, "finderwand");
+    	GameRegistry.registerItem(bridgeWand, "bridgewand");
+    	GameRegistry.registerBlock(burlapStone, "BurlapStone");
+    	GameRegistry.registerWorldGenerator(genHandler, 0);
+    	MinecraftForge.EVENT_BUS.register(eventHandler);
     	
     }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        
+    	
     }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    	
+    }
+    
 }
--- forge/src/main/java/com/kcaluru/burlapbot/domaingenerator/actions/MovementAction.java
+++
@@ -1,5 +0,0 @@
-package com.kcaluru.burlapbot.domaingenerator.actions;
-
-public class MovementAction {
-
-}

b0e0478e68f2460b6f026c9699437425a9a3d4f5
kcaluru <krishna_aluru@brown.edu>
2015-05-25 20:30:30 +0530

    added gitignore
    Former-commit-id: 2671df36cf1be61005dd7135c575f82417624bbc

--- src/main/java/com/kcaluru/burlapbot/helper/HelperBlockSide.java
+++
@@ -1,5 +0,0 @@
-package com.kcaluru.burlapbot.helper;
-
-public enum HelperBlockSide {
-	UPPER_HALF, LOWER_HALF, ANY;
-}
--- src/main/java/com/kcaluru/burlapbot/item/ItemFilter.java
+++
@@ -1,9 +0,0 @@
-package com.kcaluru.burlapbot.item;
-
-import net.minecraft.item.ItemStack;
-
-public abstract interface ItemFilter {
-	
-	public abstract boolean matches(ItemStack paramItemStack);
-
-}

63ecc366bb5d83374ef8def2bfac6d40207270b6
Stefanie Tellex <stefie10@cs.brown.edu>
2015-06-02 20:44:36 -0400

    renaming mod to burlapcraft

--- src/main/java/edu/brown/cs/h2r/burlapcraft/BurlapCraft.java
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/BurlapCraft.java
@@ -27,7 +27,7 @@ import edu.brown.cs.h2r.burlapcraft.item.ItemFinderWand;
 public class BurlapCraft {
 	
 	// mod information
-    public static final String MODID = "burlapmod";
+    public static final String MODID = "burlapcraftmod";
     public static final String VERSION = "1.0";
     
     // items

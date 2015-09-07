b5111fe870bfc374602f46229b158d2665b03195
kcaluru <krishna_aluru@brown.edu>
2015-09-07 16:33:30 -0400

    changed version to 1.1

--- src/main/java/edu/brown/cs/h2r/burlapcraft/BurlapCraft.java
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/BurlapCraft.java
@@ -35,7 +35,7 @@ public class BurlapCraft {
 	
 	// mod information
     public static final String MODID = "burlapcraftmod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     // items
     

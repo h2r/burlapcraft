9d196cfa95ce9db1c00ebc1d5aed41c7e6cc3e8b
kcaluru <krishna_aluru@brown.edu>
2015-11-04 14:09:34 -0500

    fixed PFs, removed domaingeneratorreal and added MinecraftEnvironment

---
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/MinecraftEnvironment.java
@@ -0,0 +1,40 @@
+package edu.brown.cs.h2r.burlapcraft;
+
+import burlap.oomdp.core.states.State;
+import burlap.oomdp.singleagent.GroundedAction;
+import burlap.oomdp.singleagent.environment.Environment;
+import burlap.oomdp.singleagent.environment.EnvironmentOutcome;
+
+public class MinecraftEnvironment implements Environment {
+
+	@Override
+	public State getCurrentObservation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EnvironmentOutcome executeAction(GroundedAction ga) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getLastReward() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isInTerminalState() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resetEnvironment() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/edu/brown/cs/h2r/burlapcraft/domaingenerator/propositionalfunction/PFBlockIsShape.java
+++ src/main/java/edu/brown/cs/h2r/burlapcraft/domaingenerator/propositionalfunction/PFBlockIsShape.java
@@ -1,9 +1,9 @@
 package edu.brown.cs.h2r.burlapcraft.domaingenerator.propositionalfunction;
 
 import burlap.oomdp.core.Domain;
-import burlap.oomdp.core.ObjectInstance;
+import burlap.oomdp.core.objects.ObjectInstance;
 import burlap.oomdp.core.PropositionalFunction;
-import burlap.oomdp.core.State;
+import burlap.oomdp.core.states.State;
 import edu.brown.cs.h2r.burlapcraft.helper.HelperActions;
 import edu.brown.cs.h2r.burlapcraft.helper.HelperNameSpace;
 
@@ -17,7 +17,7 @@ public class PFBlockIsShape extends PropositionalFunction {
 	}
 
 	@Override
-	public boolean isTrue(State s, String[] params) {
+	public boolean isTrue(State s, String... params) {
 		ObjectInstance block = s.getObject(params[0]);
 		int blockType = block.getIntValForAttribute(HelperNameSpace.ATBTYPE);
 		return HelperActions.blockShapeMap.get(blockType).equals(this.shape);
